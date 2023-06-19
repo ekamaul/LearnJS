e79872c3fb62c34bfb7d79bb7076b9952e5c021f
ekamaul <ekamaher@gmail.com>
2023-06-19 11:49:53 +0700

    bikin undian sederhana

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const isiRan = document.getElementById('ran')
+const stpbtn = document.getElementById('stpbtn')
+const strbtn = document.getElementById('strbtn')
 
 
 
 
 function acaknomor() {
     randomNum = Math.random() ;
-    isiRan.innerHTML = Math.round(randomNum * 100) ;
+    isiRan.innerHTML = Math.round(randomNum * 1000) ;
     console.log(Math.round(randomNum * 100))
 
 }
 
 
-setInterval( acaknomor, 100)
+
+function undi() {
+    acaknum = setInterval( acaknomor, 50);
+}
+
+
+
+function stopbtn() {
+    clearInterval(acaknum)
+}
+
+
+
+
+stpbtn.addEventListener("click", stopbtn)
+strbtn.addEventListener("click", undi)
+
+
+
+
 
 
 
