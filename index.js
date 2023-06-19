7adb312f15c29db3896c219b6648a68d197180c5
ekamaul <ekamaher@gmail.com>
2023-06-19 15:55:10 +0700

    menambah submit username dan password

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const isiRan = document.getElementById('ran')
 const stpbtn = document.getElementById('stpbtn')
 const strbtn = document.getElementById('strbtn')
+const tampilkanbtn = document.getElementById('btn2')
+const box1 = document.getElementById('box1')
+const btn2 = document.getElementById('btn2')
 
 
+
+const username = "eka"
+const password = "joss23"
 
 
 function acaknomor() {
@@ -24,11 +30,28 @@ function stopbtn() {
     clearInterval(acaknum)
 }
 
+function tampilkanbox() {
+    box1.style.display="block"
+}
+
+function verifikasi() {
+    valueuser = document.getElementById('userinput').value
+    valuepass = document.getElementById('passinput').value
+    if (valueuser == username && valuepass == password) {
+        document.getElementById('btn2').style.display = "block";
+        alert("Selamat, Password dan Username Benar")
+    }
+    else {
+        alert("Password atau Username Salah")
+    }
+}
 
 
 
 stpbtn.addEventListener("click", stopbtn)
 strbtn.addEventListener("click", undi)
+btn2.addEventListener("click", tampilkanbox )
+document.getElementById('submitbtn').addEventListener("click", verifikasi)
 
 
 
